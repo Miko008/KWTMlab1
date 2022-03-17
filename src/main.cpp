ea28ad05ab99c553967206a03d62bc9bc7462313
student <student@mini-iris-06.local>
2022-03-17 11:21:22 +0100

    Bib added

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
-int main() { /*...*/ return 0; }
+#include <iostream>
+
+#include "trygonometria.h"
+
+int main()
+{
+    std::cout<<"hello world\n";
+    
+    std::cout<<degreemath::sin(90)<<std::endl;
+    
+    std::cout<<degreemath::sin(30)<<std::endl;
+    
+    return 0;
+}
